639ed807ccfdfc1e19713de34e909417ffd516c1
WagnerSolak <wwwagnerww@gmail.com>
2021-07-22 12:54:49 -0300

    commit inicial

---
+++ src/test/java/com/solak/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solak.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
