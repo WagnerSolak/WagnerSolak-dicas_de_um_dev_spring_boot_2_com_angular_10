a2c50b4f88b7328d19af3943107816a5a6ac2de0
WagnerSolak <wwwagnerww@gmail.com>
2021-07-23 10:59:32 -0300

    POST para Livro

--- src/main/java/com/solak/bookstore/resources/LivroResource.java
+++ src/main/java/com/solak/bookstore/resources/LivroResource.java
@@ -1,15 +1,21 @@
 package com.solak.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.solak.bookstore.domain.Livro;
 import com.solak.bookstore.dtos.LivroDTO;
@@ -35,6 +41,29 @@ public class LivroResource {
 		List<Livro> list = service.findAll(id_cat);
 		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj){
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+	
+	// livro deve ter ao menos 1 categoria, por isso usa-se o defaultValue = "0
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0")Integer id_cat,
+			@RequestBody Livro obj){
+		Livro newObj = service.create(id_cat,obj);
+		// retornando a URI
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
 	}
 
 }
--- src/main/java/com/solak/bookstore/service/LivroService.java
+++ src/main/java/com/solak/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.solak.bookstore.domain.Categoria;
 import com.solak.bookstore.domain.Livro;
 import com.solak.bookstore.repositories.LivroRepository;
 import com.solak.bookstore.service.exceptions.ObjectNotFoundException;
@@ -28,5 +29,28 @@ public class LivroService {
 		// verificar se a catagoria existe
 		categoriaService.findById(id_cat);
 		return repository.findAllByCategoria(id_cat); // id_cat: id passado pelo user
+	}
+
+	public Livro update(Integer id, Livro obj) {
+		//verificar se existe o livro
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	//metodo para que quem chama não fique com muitas informações
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
+		
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		// setar obj como nulo para nao ocorrer erro
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat); // aqui temos a Categoria
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
