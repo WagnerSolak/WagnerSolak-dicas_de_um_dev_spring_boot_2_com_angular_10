2ee464e94d60c4fc51d27c87dcf3e0093fd98fbf
WagnerSolak <wwwagnerww@gmail.com>
2021-07-23 10:24:02 -0300

    listando todos os livros por categoria

--- src/main/java/com/solak/bookstore/service/LivroService.java
+++ src/main/java/com/solak/bookstore/service/LivroService.java
@@ -1,5 +1,6 @@
 package com.solak.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,17 @@ public class LivroService {
 	@Autowired
 	private LivroRepository repository;
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não econtrado! id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		// verificar se a catagoria existe
+		categoriaService.findById(id_cat);
+		return repository.findAllByCategoria(id_cat); // id_cat: id passado pelo user
 	}
 }
